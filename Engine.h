ba72d8e0cda9d40ddbe72e7a48a31b2da412c5cb
Lemond01 <sergio.al02837481@gmail.com>
2023-05-16 20:50:43 -0600

    Update2

--- Engine.h
+++ Engine.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <windows.h>
 #include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
 #include <xnamath.h>
+#include "resource.h"
 
 struct SimpleVertex
 {
@@ -25,8 +29,11 @@ struct CBChangesEveryFrame
     XMFLOAT4 vMeshColor;
 };
 
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow);
 HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
 HRESULT InitDevice();
 void CleanupDevice();
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void Render();
+HRESULT CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
